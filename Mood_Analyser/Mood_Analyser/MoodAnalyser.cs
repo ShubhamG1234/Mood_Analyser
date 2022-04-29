c3626641f6a69762d3b55b0351281e5679dfb782
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-04-29 20:36:16 +0530

    uc2

--- Mood_Analyser/Mood_Analyser/MoodAnalyser.cs
+++ Mood_Analyser/Mood_Analyser/MoodAnalyser.cs
@@ -6,29 +6,33 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyzerProblem
 {
-    public enum Mood
-    {
-        Happy,
-        sad
-    }
-
     public class MoodAnalyzer
     {
-        public string CheckMood(string msg)
+        public string message;  //instance variable
+        public MoodAnalyzer(string message)
         {
-            //msg : Kunal  is Happy for his new Bike
-            //msg : Kunal  is Sad after getting  fail in exam
+            this.message = message;
+        }
 
-            if (msg.Contains("happy", StringComparison.OrdinalIgnoreCase))
+        //uc-2 handling exceptions
+        public string AnalyzeMood()    //Analyzer method find mood
+        {
+            try
+            {
+                if (message.ToLower().Contains("sad"))
+                {
+                    return "sad";
+                }
+                else
+                {
+                    return "happy";
+                }
+            }
+            catch (NullReferenceException)
             {
                 return "happy";
             }
-            else
-            {
-                return "sad";
-            }
+
         }
     }
-
-
 }
